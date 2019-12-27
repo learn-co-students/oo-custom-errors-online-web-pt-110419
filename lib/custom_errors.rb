4188202b583bcff196cd458b1dfc083932a75c17
Nick Solonyy <nicksolony@gmail.com>
2019-12-27 04:59:31 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,30 +1,23 @@
 class Person
   attr_accessor :partner, :name
-  
-  class PartnerError < StandardError
-  end
-  
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
-  
-  def 
-
-class PartnerError < StandardError
+ 
+  class PartnerError < StandardError
   end
-
-
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
